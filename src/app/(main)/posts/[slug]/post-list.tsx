7f7bba04a5b6bc037d2f8039f4d7dcb1c6a51e82
Zulzario Zaeri <ytryo789@gmail.com>
2026-01-31 17:58:07 +0800

    feat: responsive in mobile

--- src/app/(main)/posts/[slug]/post-list.tsx
+++ src/app/(main)/posts/[slug]/post-list.tsx
@@ -1,10 +1,7 @@
 "use client"
 
-import axios from "@/lib/axios";
-import { APIPATHS } from "@/lib/constants";
 import { formatDateWithDayName } from "@/lib/utils";
 import { usePostStore } from "@/store/usePostStore";
-import { Post } from "@/types/model";
 
 import Link from "next/link";
 import React, { FC } from "react";
@@ -22,20 +19,20 @@ export const PostList: FC<IPostListProps> = (props) => {
         <>
             {posts.map((post) => {
                 return (
-                    <Link href={`/posts/${post.slug}`} className="flex items-center gap-5 mb-6" key={post.slug}>
+                    <Link href={`/posts/${post.slug}`} className="flex flex-col md:flex-row items-start md:items-center gap-5 mb-6 group" key={post.slug}>
                         {post.thumbnail && (
-                            <div className="w-2/6">
+                            <div className="w-full md:w-2/6">
                                 <img
                                     src={post.thumbnail}
                                     alt={post.title}
-                                    className="rounded-lg w-full object-cover aspect-video"
+                                    className="rounded-lg w-full object-cover aspect-video group-hover:opacity-90 transition-opacity"
                                 />
                             </div>
                         )}
-                        <div className="w-4/6">
+                        <div className="w-full md:w-4/6">
                             <p className="text-muted-foreground">{formatDateWithDayName(post.created_at)}</p>
-                            <h1 className="text-xl font-bold">{post.title}</h1>
-                            <p className="text-sm text-muted-foreground">{post.description}</p>
+                            <h1 className="text-xl font-bold group-hover:text-primary transition-colors">{post.title}</h1>
+                            <p className="text-sm text-muted-foreground line-clamp-2">{post.description}</p>
                         </div>
                     </Link>
                 )
